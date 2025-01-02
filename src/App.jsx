657d31417bbbdcdc845f8149f61ce19cbb80c3a0
0815beck <nicobeck97@gmail.com>
2025-01-02 15:23:28 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import UeberMich from "./pages/UeberMich"
+
+function App() {
+  return (
+    <UeberMich />
+  )
+}
+
+export default App
---
+++ src/pages/UeberMich.jsx
@@ -0,0 +1,21 @@
+function UeberMich() {
+  return (
+    <main>
+        <h1>Über mich</h1>
+        <p>
+            Ich bin ein:e Ex-Mathematikstudent:in aus Berlin. Mein besonderes Interesse gilt funktionalen
+            Programmiersprachen, kategorischer Logik und der Semantik von Typentheorie. Wenn du dich auf meiner Webseite umschaust, 
+            kannst du viele Quellen, Links und Bücher zu dem Thema finden, die ich über die letzten Jahre angesammelt habe. 
+        </p>
+        <p>
+            Gerade bin ich auf der Suche nach einem Job als Softwareentwickler:in. Ich hab das letzte Jahr geübet,
+            also, wenn du Interesse hast und es mit einem sehr motiviertem Junior versuchen möchtest, schreib mir gern. Was 
+            ich gerade mache, wenn ich nicht auf Jobsuche bin, ist über Datebanksysteme zu lernen. Ich werde bestimmt auch ein 
+            paar Blogposts dazu schreiben, wenn es sich ergibt, um meinen Lernprozess zu dokumentieren, aber das ist hier alles
+            noch im entstehen.
+        </p>
+    </main>
+  )
+}
+
+export default UeberMich
