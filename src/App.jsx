165ae1a30af0cb30daedf9ca594e7a69e7ef8ddc
0815beck <nicobeck97@gmail.com>
2025-01-07 15:03:16 +0100

    changed home and default route to /

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,59 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import { useState } from "react"
-import UeberMich from "./pages/UeberMich"
-import Navbar from "./components/Navbar"
-import Texte from "./pages/Texte"
-import useIsMd from "./hooks/useIsMd" 
-import MobileNavbar from "./components/MobileNavbar"
-import Blog from "./pages/Blog"
-import Kontakt from "./pages/Kontakt"
-import Resume from "./pages/Resume"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+import UeberMich from "./pages/UeberMich";
+import Navbar from "./components/Navbar";
+import Texte from "./pages/Texte";
+import useIsMd from "./hooks/useIsMd";
+import MobileNavbar from "./components/MobileNavbar";
+import Blog from "./pages/Blog";
+import Kontakt from "./pages/Kontakt";
+import Resume from "./pages/Resume";
 
 function App() {
-  const isMd = useIsMd()
-  const [mobileNavbarIsOpen, setMobileNavbarIsOpen] = useState(false)
+  const isMd = useIsMd();
+  const [mobileNavbarIsOpen, setMobileNavbarIsOpen] = useState(false);
 
   const closeMobileMenuIfOpen = () => {
-    if (mobileNavbarIsOpen) { setMobileNavbarIsOpen(false) }
-  }
+    if (mobileNavbarIsOpen) {
+      setMobileNavbarIsOpen(false);
+    }
+  };
 
   return (
     <>
-      {isMd ? <Navbar /> : <MobileNavbar 
+      {isMd ? (
+        <Navbar />
+      ) : (
+        <MobileNavbar
           isOpen={mobileNavbarIsOpen}
-          setIsOpen={setMobileNavbarIsOpen}/>
-      }
+          setIsOpen={setMobileNavbarIsOpen}
+        />
+      )}
       <Routes>
-        <Route path="/home" element={<UeberMich onClick={closeMobileMenuIfOpen}/>}/>
-        <Route path="/texte" element={<Texte onClick={closeMobileMenuIfOpen}/>}/>
-        <Route path="/blog" element={<Blog onClick={closeMobileMenuIfOpen}/>}/>
-        <Route path="/resume" element={<Resume onClick={closeMobileMenuIfOpen}/>}/>
-        <Route path="/kontakt" element={<Kontakt onClick={closeMobileMenuIfOpen}/>}/>
-        <Route path="*" element={<Navigate to="/home" replace />}/>
+        <Route
+          path="/"
+          element={<UeberMich onClick={closeMobileMenuIfOpen} />}
+        />
+        <Route
+          path="/texte"
+          element={<Texte onClick={closeMobileMenuIfOpen} />}
+        />
+        <Route
+          path="/blog"
+          element={<Blog onClick={closeMobileMenuIfOpen} />}
+        />
+        <Route
+          path="/resume"
+          element={<Resume onClick={closeMobileMenuIfOpen} />}
+        />
+        <Route
+          path="/kontakt"
+          element={<Kontakt onClick={closeMobileMenuIfOpen} />}
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
